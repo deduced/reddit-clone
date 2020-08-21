61b2835fba606c5f523e779415f4adbed417fc8f
Charlie Astrada <carlosastrada@gmail.com>
2020-08-21 20:16:48 -0300

    Set up mikroOrm base connectivity
    - config file extracted to own file for running CLI easily;
    - Post defines entity with specific types for dates and title;
    - package.json contains configPaths for mikroOrm config file;
    - initial migration run using npx mikro-orm migration:create

--- server/dist/mikro-orm.config.js
+++ server/dist/mikro-orm.config.js
@@ -1,10 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const Post_1 = require("./entities/Post");
+const path_1 = __importDefault(require("path"));
 exports.default = {
+    migrations: {
+        path: path_1.default.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
     dbName: "reddit_clone",
     debug: process.env.NODE_ENV !== "production",
     entities: [Post_1.Post],
     type: "postgresql",
+    user: "charlieastrada",
 };
 //# sourceMappingURL=mikro-orm.config.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main().catch((err) => console.error(err));

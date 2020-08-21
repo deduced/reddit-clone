939cb087efd90776b287b237a4759acbc7aae608
Charlie Astrada <carlosastrada@gmail.com>
2020-08-21 20:21:01 -0300

    Fix bug with migrations in dist not being cleared out to fix previous error.
    When we modified Post to have specific types, typescript did not clear out dist, so the old migration file was being attempted. Clearing out dist fixes the issue.

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
-
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, { title: "my first post" });
   await orm.em.persistAndFlush(post);
 };

3738542c986560f4b4f36bf77df92d37ef7b96c0
Charlie Astrada <carlosastrada@gmail.com>
2020-08-24 15:08:20 -0300

    Setup graphql apollo server with placeholder resolver
    Resolver just prints hello world equivalent for testing out the integration. Apollo server set up as middleware for express. Resolver uses type-graphql library and a simple hello.ts example to test the integration was created. This will be removed later.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
 // import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  //   const post = orm.em.create(Post, { title: "my first post" });
-  //   await orm.em.persistAndFlush(post);
 
-  //   const posts = await orm.em.find(Post, {});
-  //   console.log(posts);
+  const port = 3000;
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  //create graphql endpoint on express
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+  });
 };
 
 main().catch((err) => console.error(err));

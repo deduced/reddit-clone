bc499ac1a885069360601220e9ebba2bec911594
Charlie Astrada <carlosastrada@gmail.com>
2020-08-25 19:51:47 -0300

    Add User entity
    User entity includes username, password (which is not queryable), createdAt, and updatedAt. Eventually, more fields will be added.

---
+++ server/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
+
+@ObjectType() //convert class into graphql type with this decorator and Field decorator for individual fields
+@Entity()
+export class Post {
+  @Field(() => Int) //type will be inferred but we can specifiy anyways.
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String) //Type needed as it cannot be inferred by default for date. if not available, NoExplicitTypeError will occur
+  @Property({ type: "date" })
+  createdAt = new Date();
+
+  @Field(() => String) //Type needed as it cannot be inferred by default for date
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Field() // no specific type needed as it can be inferred
+  @Property({ type: "text", unique: true })
+  username!: string;
+
+  @Property({ type: "text" })
+  password!: string;
+}

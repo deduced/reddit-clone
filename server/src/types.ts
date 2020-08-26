212f7fa53ed2be592a493802264027a9fa810c8a
Charlie Astrada <carlosastrada@gmail.com>
2020-08-26 16:37:21 -0300

    Update login function in user resolver to store userId to session

--- server/src/types.ts
+++ server/src/types.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request;
+  req: Request & { session: Express.Session }; //set session type join as req will always be set
   res: Response;
 };

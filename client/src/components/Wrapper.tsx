14b9256d1ebdf8a8477d8feb4ed36fe0731461ab
Charlie Astrada <carlosastrada@gmail.com>
2020-08-28 17:52:15 -0300

    Set up register form with formik
    - create InputField component to keep things DRY

---
+++ client/src/components/Wrapper.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box } from "@chakra-ui/core";
+
+interface WrapperProps {
+  variant?: "small" | "regular";
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => {
+  return (
+    <Box
+      maxW={variant === "regular" ? "800px" : "400px"}
+      mt={8}
+      mx="auto"
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
+};
+
+export default Wrapper;
---
+++ client/src/pages/register.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/core";
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting, handleChange, values }) => (
+          <Form>
+            <InputField
+              label="Username"
+              name="username"
+              placeholder="username"
+            />
+            <Box mt={4}>
+              <InputField
+                label="Password"
+                name="password"
+                placeholder="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              variantColor="teal"
+              type="submit"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;

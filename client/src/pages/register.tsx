8e786a29c9f8a43abbe8f04fab4427c340b2b710
Charlie Astrada <carlosastrada@gmail.com>
2020-09-04 18:00:10 -0300

    Add error-handling to /register
    When user attempts to register, check for errors from graphql endpoint and mark the field with returned error. For example, if the username length validation does not pass, mark the username field and display the error message below the filed. If registration was successful, redirect the user to the "/" route.

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -11,21 +11,33 @@ import {
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [_, register] = useRegisterMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register({
             username: values.username,
             password: values.password,
           });
+
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            //if no errors, and we get a user after succcessful registration, redirect to root
+            router.push("/");
+          } else {
+            console.error(response);
+          }
         }}
       >
         {({ isSubmitting, handleChange, values }) => (

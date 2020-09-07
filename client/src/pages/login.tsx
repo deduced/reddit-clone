2057b6cac60563f96cc12a8875e5ab9d359cc2e3
Charlie Astrada <carlosastrada@gmail.com>
2020-09-07 12:22:36 -0300

    Add login page
    Login page uses login function from graphql endpoint. Currently, merely redirects use to "/" path.

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/core";
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [_, login] = useLoginMutation();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({
+            username: values.username,
+            password: values.password,
+          });
+
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            //if no errors, and we get a user after succcessful registration, redirect to root
+            router.push("/");
+          } else {
+            console.error(response);
+          }
+        }}
+      >
+        {({ isSubmitting, handleChange, values }) => (
+          <Form>
+            <InputField
+              label="Username"
+              name="username"
+              placeholder="username"
+            />
+            <Box mt={4}>
+              <InputField
+                label="Password"
+                name="password"
+                placeholder="password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              variantColor="teal"
+              type="submit"
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;

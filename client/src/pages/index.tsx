91d7901114bff4fd4214067ed576113eba244320
Charlie Astrada <carlosastrada@gmail.com>
2020-09-09 01:35:27 -0300

    Show all post titles
    - Set up index using SSR

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,13 +2,21 @@ import { withUrqlClient } from "next-urql";
 
 import { NavBar } from "../components/NavBar";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
 const Index = () => {
+  const [{ data }] = usePostsQuery();
+
   return (
     <>
       <NavBar />
+      {!data ? (
+        <div>...loading</div>
+      ) : (
+        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+      )}
     </>
   );
 };
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);

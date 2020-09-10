5659906939ccc96901042905c58daacfabc1e25d
Charlie Astrada <carlosastrada@gmail.com>
2020-09-10 15:50:45 -0300

    Add sendEmail util
    sendEmail uses nodemailer to send email. It will be used in the resolver.

---
+++ server/src/utils/sendEmail.ts
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+
+// async..await is not allowed in global scope, must use a wrapper
+export async function sendEmail(to: string, subject: string, html: string) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  //   const testAccount = await nodemailer.createTestAccount();
+  //   console.log("Test account for email: ", testAccount);
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "gb455p3cstrpymxc@ethereal.email", // generated ethereal user
+      pass: "JwnypEDbFvytdrSg6K", // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Losy Poo 👻" <foo@example.com>', // sender address
+    to, // list of receivers
+    subject, // Subject line
+    // text, // plain text body
+    html, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}

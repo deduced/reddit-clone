9d637ab3e1b7c21079e69bbc901f769c6839a5f0
Charlie Astrada <carlosastrada@gmail.com>
2020-09-16 00:02:44 -0300

    Add forgot-password page
    Forgot Password page will  be where the user enters their email to get hte reset password link. The user can arrive to the forgot-password page via the login "forgot password?" link. When the user enters an email the forgotPassword mutation will invoke the functionality to send the password.
    
    - add hook/types for forgotPassword

---
+++ client/src/pages/forgot-password.tsx
@@ -0,0 +1,52 @@
+import { Box, Button } from "@chakra-ui/core";
+import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
+import React, { useState } from "react";
+import InputField from "../components/InputField";
+import Wrapper from "../components/Wrapper";
+import { useForgotPasswordMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+
+const forgotPassword: React.FC<{}> = ({}) => {
+  const [isComplete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
+        }}
+      >
+        {({ isSubmitting }) =>
+          isComplete ? (
+            <Box>
+              If an account with that email exists, we sent you a reset password
+              email.
+            </Box>
+          ) : (
+            <Form>
+              <InputField
+                label="Email"
+                name="email"
+                placeholder="email"
+                type="email"
+              />
+              <Button
+                mt={2}
+                isLoading={isSubmitting}
+                variantColor="teal"
+                type="submit"
+              >
+                Forgot password
+              </Button>
+            </Form>
+          )
+        }
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(forgotPassword);

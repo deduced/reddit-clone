31ada2f5e6fb210b5a3aa516d92f04105d22d382
Charlie Astrada <carlosastrada@gmail.com>
2020-09-23 15:02:53 -0300

    Add Layout to index
    - Refactor index to work properly with Layout by removing NavBar in index.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,19 +3,24 @@ import { withUrqlClient } from "next-urql";
 import { NavBar } from "../components/NavBar";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { Link } from "@chakra-ui/core";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
 
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <NextLink href="create-post">
+        <Link>create post</Link>
+      </NextLink>
       {!data ? (
         <div>...loading</div>
       ) : (
         data.posts.map((post) => <div key={post.id}>{post.title}</div>)
       )}
-    </>
+    </Layout>
   );
 };
 

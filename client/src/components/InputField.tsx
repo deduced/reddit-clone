8a770ee971a2856bf60153a81aa89aa68fe56909
Charlie Astrada <carlosastrada@gmail.com>
2020-09-23 15:03:51 -0300

    Update InputField component to use TextArea
    Input refactored to use input or text area. A prop added to component for textarea (boolean) which will make the componnent use a textarea

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -5,23 +5,31 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/core";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea; //if optional textarea, set component accordingly
+  }
+
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={Boolean(error)}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
+      <InputOrTextarea
         {...field}
         {...props}
         id={field.name}

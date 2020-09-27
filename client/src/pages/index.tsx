821bea7168a32823605409cd32f5fbd14838e38d
Charlie Astrada <carlosastrada@gmail.com>
2020-09-27 17:21:41 -0300

    Style index (Posts List)
    Style cards with title and textSnippet.
    
    -Add state management conditionals for loading and data.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,27 +1,51 @@
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
-
-import { createUrqlClient } from "../utils/createUrqlClient";
-import { usePostsQuery } from "../generated/graphql";
-import { Layout } from "../components/Layout";
-import { Link } from "@chakra-ui/core";
 import NextLink from "next/link";
+import React from "react";
+import { Layout } from "../components/Layout";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching: isLoading }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
 
+  if (!isLoading && !data) {
+    return (
+      <Text color={"#ff000"}>Something went wrong! Reload and try again</Text>
+    );
+  }
+
   return (
     <Layout>
-      <NextLink href="create-post">
-        <Link>create post</Link>
-      </NextLink>
-      {!data ? (
-        <div>...loading</div>
-      ) : (
-        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+      <Flex justifyContent="space-between" alignItems="baseline">
+        <Heading mb={4}>Reddit-Clone</Heading>
+        <NextLink href="create-post">
+          <Link>create post</Link>
+        </NextLink>
+      </Flex>
+      {isLoading && !data && <div>loading...</div>}
+
+      {!isLoading && data && (
+        <Stack spacing={8}>
+          {data.posts.map((post) => (
+            <Box key={post.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+
+      {data && (
+        <Flex justifyContent="center">
+          <Button isLoading={isLoading} my={8}>
+            load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );

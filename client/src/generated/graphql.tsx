6825b81c41fdb3d644ffbf4718fa610afe7a51c4
Charlie Astrada <carlosastrada@gmail.com>
2020-10-06 14:45:06 -0300

    Update posts.graphql to include creator (user)
    creator (user  who created post) data will be used to show the username of hte creator on a post. We grab the id to fit standardized caching protocols.

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -44,6 +44,7 @@ export type Post = {
   text: Scalars['String'];
   points: Scalars['Float'];
   creatorId: Scalars['Float'];
+  creator: User;
   createdAt: Scalars['String'];
   updatedAt: Scalars['String'];
   textSnippet: Scalars['String'];
@@ -60,6 +61,7 @@ export type User = {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  vote: Scalars['Boolean'];
   createPost: Post;
   updatePost?: Maybe<Post>;
   deletePost: Scalars['Boolean'];
@@ -68,6 +70,12 @@ export type Mutation = {
   register: UserResponse;
   login: UserResponse;
   logout: Scalars['Boolean'];
+};
+
+
+export type MutationVoteArgs = {
+  value: Scalars['Int'];
+  postId: Scalars['Int'];
 };
 
 
@@ -249,6 +257,10 @@ export type PostsQuery = (
     & { posts: Array<(
       { __typename?: 'Post' }
       & Pick<Post, 'id' | 'title' | 'textSnippet' | 'points' | 'createdAt' | 'updatedAt'>
+      & { creator: (
+        { __typename?: 'User' }
+        & Pick<User, 'id' | 'username'>
+      ) }
     )> }
   ) }
 );
@@ -366,6 +378,10 @@ export const PostsDocument = gql`
       points
       createdAt
       updatedAt
+      creator {
+        id
+        username
+      }
     }
   }
 }

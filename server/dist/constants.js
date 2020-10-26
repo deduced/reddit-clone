98284ed588c4c34f6aa48368329176cce5483fc7
Charlie Astrada <carlosastrada@gmail.com>
2020-10-26 17:50:05 -0300

    REcursive removal of dist
    Not needed as it will be generated on server.

--- server/dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FORGET_PASSWORD_PREFIX = exports.COOKIE_NAME = void 0;
-exports.COOKIE_NAME = "qid";
-exports.FORGET_PASSWORD_PREFIX = "forget-password:";
-//# sourceMappingURL=constants.js.map
--- server/dist/utils/sleep.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-exports.sleep = (ms) => new Promise((res) => setTimeout(res, ms));
-//# sourceMappingURL=sleep.js.map
